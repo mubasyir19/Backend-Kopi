8e92b54cbf7b1dd080873a8ccd63dbe54c54e156
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2025-07-02 11:42:38 +0700

    delete comment and fix reponse

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -15,7 +15,7 @@ export const login = async (req: Request, res: Response): Promise<Response | any
 
   try {
     const handleLogin = await authSerive.login({ username, password });
-    res.status(400).json(
+    return res.status(400).json(
       ResponseUtil.success(
         {
           id: handleLogin.user.id,
@@ -25,18 +25,7 @@ export const login = async (req: Request, res: Response): Promise<Response | any
         'Login successfully'
       )
     );
-
-    // return res.status(200).json({
-    //   status: 200,
-    //   message: 'login successfully',
-    //   data: {
-    // id: handleLogin.user.id,
-    // role: handleLogin.user.name,
-    // access_token: handleLogin.jwtToken,
-    //   },
-    // });
   } catch (error: any) {
-    console.log('Error = ', error);
     return res.status(500).json({
       status: 500,
       message: error.message,
@@ -49,45 +38,9 @@ export const register = async (req: Request, res: Response): Promise<Response | 
   const { name, username, password, role } = req.body;
 
   try {
-    // const checkUser = await prisma.user.findFirst({
-    //   where: {
-    //     username,
-    //   },
-    // });
-
-    // if (checkUser) {
-    //   return res.status(400).json({
-    //     status: 400,
-    //     message: 'An account has been registered',
-    //     data: null,
-    //   });
-    // } else {
-    //   const salt = await bcrypt.genSalt();
-    //   const hashedPassword = await bcrypt.hash(password, salt);
-
-    //   const newUser = await prisma.user.create({
-    //     data: {
-    //       name: name,
-    //       username: username,
-    //       password: hashedPassword,
-    //       role: role,
-    //     },
-    //   });
-
-    //   return res.status(201).json({
-    //     status: 201,
-    //     message: 'an account successfully register',
-    //     data: newUser,
-    //   });
-    // }
-
     const newUser = await authSerive.register({ name, username, password, role });
 
-    return res.status(201).json({
-      status: 201,
-      message: 'an account successfully register',
-      data: newUser,
-    });
+    return res.status(400).json(ResponseUtil.success(newUser, 'An account successfully register'));
   } catch (error: any) {
     return res.status(500).json({
       status: 500,
